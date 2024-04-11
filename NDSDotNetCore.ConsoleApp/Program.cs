5ff43d830ab9641eaeef92a3a69d890bdaf0fb32
Nadi <nadisoepaing9@gmail.com>
2024-04-11 13:46:20 +0630

    AdoDotNet complete

--- NDSDotNetCore.ConsoleApp/Program.cs
+++ NDSDotNetCore.ConsoleApp/Program.cs
@@ -1,10 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Data;
 using System.Data.SqlClient;
+using NDSDotNetCore.ConsoleApp;
 
 Console.WriteLine("Hello, World!");
 
-SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+/*SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
 stringBuilder.DataSource = "NADI";
 stringBuilder.InitialCatalog = "NDSDotNetCore";
 stringBuilder.UserID = "sa";
@@ -29,6 +30,14 @@ foreach (DataRow dr in dt.Rows)
     Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
     Console.WriteLine("Blog Content => " + dr["BlogContent"]);
     Console.WriteLine("------------------------------------");
-}
+}*/
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create("title", "author", "content");
+//adoDotNetExample.Update(12, "test title", "test author", "test content");
+//adoDotNetExample.Delete(12);
+adoDotNetExample.Edit(1002);
+adoDotNetExample.Edit(1);
 
 Console.ReadKey();
