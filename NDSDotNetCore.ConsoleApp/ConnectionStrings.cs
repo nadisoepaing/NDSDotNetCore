46f4cf10ba726d6c9a891f00855df7c2cc46766a
Nadi <nadisoepaing9@gmail.com>
2024-04-23 16:45:00 +0630

    Complete

---
+++ NDSDotNetCore.ConsoleApp/ConnectionStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDSDotNetCore.ConsoleApp
+{
+    internal class ConnectionStrings
+    {
+
+        public static SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder() 
+        { 
+            DataSource = "NADI",
+            InitialCatalog = "NDSDotNetCore",
+            UserID = "sa",
+            Password = "sa@123",
+
+        };
+    }
+}
--- NDSDotNetCore.ConsoleApp/Program.cs
+++ NDSDotNetCore.ConsoleApp/Program.cs
@@ -32,12 +32,15 @@ foreach (DataRow dr in dt.Rows)
     Console.WriteLine("------------------------------------");
 }*/
 
-AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
 //adoDotNetExample.Read();
 //adoDotNetExample.Create("title", "author", "content");
 //adoDotNetExample.Update(12, "test title", "test author", "test content");
 //adoDotNetExample.Delete(12);
-adoDotNetExample.Edit(1002);
-adoDotNetExample.Edit(1);
+//adoDotNetExample.Edit(1002);
+//adoDotNetExample.Edit(1);
+
+DapperExample dapperExample = new DapperExample();
+dapperExample.Run();
 
 Console.ReadKey();
